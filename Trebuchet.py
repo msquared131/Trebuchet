9e93353144f64a35e6971904de447edaf1031dd4
Matthew Metzger <matthew.metzger131@gmail.com>
2024-10-06 14:02:33 -0500

    Trebuchet Part 1 - cleaned up warnings

--- Trebuchet.py
+++ Trebuchet.py
@@ -1,11 +1,10 @@
 sum = 0
 
-with open("TrebuchetInput.txt") as f:
+with open("TrebuchetInput.txt", "r", encoding="utf-8") as f:
     for line in f:
         numbers = []
         for c in line:
             if c.isnumeric():
                 numbers.append(c)
         sum += int(numbers[0] + numbers[-1])
-        print("string: " + line + " value: " + numbers[0] + numbers[-1])
 print(sum)
